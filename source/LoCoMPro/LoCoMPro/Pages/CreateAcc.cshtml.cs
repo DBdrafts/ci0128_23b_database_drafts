fa4dd8555bd10a8cf25a4215c725d1bb84464eab
alr201 <e.alonsoleon@gmail.com>
2023-09-24 17:54:01 -0600

    Se agregó la página de Crear una Cuenta

---
+++ source/LoCoMPro/LoCoMPro/Pages/CreateAcc.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LoCoMPro.Pages
+{
+    public class CreateAccModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
