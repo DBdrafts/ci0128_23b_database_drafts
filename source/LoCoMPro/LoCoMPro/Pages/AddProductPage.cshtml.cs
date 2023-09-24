9472a1b7b670297dafa2a364f4dce7bd64aec4cf
Geanca Rivera <geancarlo.riverahernandez@ucr.ac.cr>
2023-09-24 10:00:49 -0600

    Se agrega página y campo del formulario para ingresar datos de un producto

---
+++ source/LoCoMPro/LoCoMPro/Pages/AddProductPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LoCoMPro.Pages
+{
+    public class AddProductPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
