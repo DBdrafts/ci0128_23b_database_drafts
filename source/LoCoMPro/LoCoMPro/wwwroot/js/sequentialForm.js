2c6d35c9193836f998bdfde922f308e992f4f2de
Geanca Rivera <geancarlo.riverahernandez@ucr.ac.cr>
2023-09-27 13:44:01 -0600

    Se añade script al formulario para debloquear los campos secuencialmente

---
+++ source/LoCoMPro/LoCoMPro/wwwroot/js/sequentialForm.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function () {
+    // Define the order of required fields in an array
+    var requiredFields = [
+        "#location",
+        "#productName",
+        "#price"
+    ];
+
+    // Initially, disable all fields except the first one (Establecimiento)
+    $(".required-field").prop("disabled", true);
+    $(requiredFields[0]).prop("disabled", false);
+
+    // Enable the next field in the sequence when the previous field is filled
+    $(".required-field").on("input", function () {
+        var $this = $(this); // Capture the current field as a jQuery object
+        if ($this.val().trim() !== "") { // Check if the current field has content
+            var currentIndex = requiredFields.indexOf("#" + $this.attr("id")); // Get the index of the current field in the array
+            if (currentIndex !== -1 && currentIndex < requiredFields.length - 1) {
+                var nextField = requiredFields[currentIndex + 1]; // Get the next field in the sequence
+                $(nextField).prop("disabled", false); // Enable the next field
+            }
+        } else {
+            // If the current field is cleared, disable the fields following in the sequence
+            var currentIndex = requiredFields.indexOf("#" + $this.attr("id")); // Get the index of the current field in the array
+            if (currentIndex !== -1) {
+                for (var i = currentIndex + 1; i < requiredFields.length; i++) {
+                    $(requiredFields[i]).prop("disabled", true); // Disable the following fields
+                    // $(requiredFields[i]).prop("disabled", true).val(""); -> If we want to clean the next fields
+                }
+            }
+        }
+    });
+});
