a7465b982a372507fa760d994aa13b0f918723ac
JulioAleRodri <JULIO.RODRIGUEZSALGUERA@ucr.ac.cr>
2023-09-28 23:41:14 -0600

    Agregando comentarios y mensajes dinámicos, eliminando error de botones de búsqueda, detalle de la barra de búsqueda

--- source/LoCoMPro/LoCoMPro/Pages/SearchPage.cshtml.cs
+++ source/LoCoMPro/LoCoMPro/Pages/SearchPage.cshtml.cs
@@ -9,24 +9,30 @@ namespace LoCoMPro.Pages
 {
     public class SearchPageModel : PageModel
     {
+        /* Determinate if the check-box was activated */
         [BindProperty]
         public bool IsChecked { get; set; }
 
+        /* Context of the data base */
         private readonly LoCoMPro.Data.LoCoMProContext _context;
 
+        /* Search Page constructor */
         public SearchPageModel(LoCoMProContext context)
         {
             _context = context;
         }
 
+        /* List of the categories that exist in the database */
         public IList<Category> Category { get; set; } = default!;
 
+        /* List of the registers that match with the search string */
         public IList<Register> Register { get; set; } = default!;
 
-
+        /* Text enters as the search attribute */
         [BindProperty(SupportsGet = true)]
         public string? SearchString { get; set; }
 
+        /* OnGet method that manage the GET request */
         public async Task OnGetAsync()
         {
             var categories = from c in _context.Categories
@@ -41,6 +47,7 @@ namespace LoCoMPro.Pages
             Register = await registros.ToListAsync();
         }
 
+        /* OnPost method that sent request */
         public IActionResult OnPost()
         {
             return Page();
