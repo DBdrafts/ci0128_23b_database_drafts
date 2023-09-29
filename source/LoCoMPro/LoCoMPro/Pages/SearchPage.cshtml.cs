79264d9744855c441c7cdb338bbd58da98b64f18
JulioAleRodri <JULIO.RODRIGUEZSALGUERA@ucr.ac.cr>
2023-09-28 21:39:15 -0600

    Se agrega la funcionalidad de búsqueda y se agrega la funcionalidad de mostrar los filtros de la base de datos

--- source/LoCoMPro/LoCoMPro/Pages/SearchPage.cshtml.cs
+++ source/LoCoMPro/LoCoMPro/Pages/SearchPage.cshtml.cs
@@ -1,5 +1,9 @@
+using LoCoMPro.Data;
+using LoCoMPro.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace LoCoMPro.Pages
 {
@@ -8,8 +12,33 @@ namespace LoCoMPro.Pages
         [BindProperty]
         public bool IsChecked { get; set; }
 
-        public void OnGet()
+        private readonly LoCoMPro.Data.LoCoMProContext _context;
+
+        public SearchPageModel(LoCoMProContext context)
         {
+            _context = context;
+        }
+
+        public IList<Category> Category { get; set; } = default!;
+
+        public IList<Register> Register { get; set; } = default!;
+
+
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            var categories = from c in _context.Categories
+                             select c;
+
+            var registros = from r in _context.Registers
+                            select r;
+
+            registros = registros.Where(x => x.ProductName.Contains(SearchString));
+
+            Category = await categories.ToListAsync();
+            Register = await registros.ToListAsync();
         }
 
         public IActionResult OnPost()
