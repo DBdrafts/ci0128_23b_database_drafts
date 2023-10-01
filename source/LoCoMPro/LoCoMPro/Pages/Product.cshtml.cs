9f0a54a03c8594b5cbb4a3404594d04aa547b45e
Omar <omar.camacho@ucr.ac.cr>
2023-09-30 23:07:37 -0600

    Conexion con bases de datos, visualización de registros y estilos modicados.

--- source/LoCoMPro/LoCoMPro/Pages/Product.cshtml.cs
+++ source/LoCoMPro/LoCoMPro/Pages/Product.cshtml.cs
@@ -41,28 +41,44 @@ namespace LoCoMPro.Pages
             requestedStoreName = searchStoreName;
 
             // Initial request for all the products in the database
-            var products = from r in _context.Products select r;
+            var products = from p in _context.Products select p;
 
             // Initial request for all the stores in the database
-            var stores = from r in _context.Stores select r;
+            var stores = from s in _context.Stores select s;
 
 
             // If the name of the productResquested is not null
             if (!string.IsNullOrEmpty(requestedProductName))
             {
-                // Delimits registers with a where the properties are same 
+                // Delimits products with a where the properties are same 
                 products = products.Where(x => x.Name != null && x.Name.Contains(requestedProductName));
             }
 
             // If the name of the storeResquested is not null
             if (!string.IsNullOrEmpty(requestedStoreName))
             {
-                // Delimits registers with a where the properties are same 
+                // Delimits stores with a where the properties are same 
                 stores = stores.Where(x => x.Name != null && x.Name.Contains(requestedStoreName));
             }
-
+            
+            // Gets the Data From Databasse 
             Product = await products.ToListAsync();
             Store = await stores.ToListAsync();
+
+            // Initial request for all the registers in the database
+            var registers = from r in _context.Registers select r;
+
+            // If the name of the propertiesRequested is not null 
+            if (!string.IsNullOrEmpty(requestedProductName) &&
+                !string.IsNullOrEmpty(requestedStoreName)) {
+
+                // Delimits registers with the same properties
+                registers = registers.Where(x => x.ProductName != null && x.ProductName.Contains(requestedProductName));
+                registers = registers.Where(x => x.StoreName != null && x.StoreName.Contains(requestedStoreName));
+            }
+
+            // Gets the Data From Databasse 
+            Register = await registers.ToListAsync();
 
         }
 
