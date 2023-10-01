3ba3d420e51f295227e01f86f414516e8fe9a1ca
Omar <omar.camacho@ucr.ac.cr>
2023-09-30 19:17:56 -0600

    Delimitación para visualización por nombre del producto y tienda

--- source/LoCoMPro/LoCoMPro/Pages/Product.cshtml.cs
+++ source/LoCoMPro/LoCoMPro/Pages/Product.cshtml.cs
@@ -18,48 +18,46 @@ namespace LoCoMPro.Pages
         }
 
         /* List of the product that exist in the database */
-        public IList<Register> register { get; set; } = default!;
+        public IList<Register> Register { get; set; } = default!;
 
         /* Amount Register Displayed */
         public int amountRegisterDisplayed { get; set; } = 3;
 
-        /* Amount total pages*/
-        public int amountPaginationPages { get; set; }
-
         /* Product resquested name */
         [BindProperty(SupportsGet = true)]
         public string? requestedProductName { get; set; }
+        public string? requestedStoreName { get; set; }
+        public string? ProductBrand { get; set; }
+        public string? ProductModel { get; set; }
+        public string? ProductName { get; set; }
+        public string? CantonName { get; set; }
+        public string? ProvinceName { get; set; }
 
-        public async Task OnGetAsync() 
+
+
+        public async Task OnGetAsync(string searchProductName, string searchStoreName)
         {
+            // Requested Name to search
+            requestedProductName = searchProductName;
+            requestedStoreName = searchStoreName;
             // Initial request that request all the registers in the database
             var registers = from r in _context.Registers select r;
 
             // If the name of th productResquested is not null
-            if (!string.IsNullOrEmpty(requestedProductName) ) 
+            if (!string.IsNullOrEmpty(requestedProductName))
             {
                 // Delimits registers with a where the properties are same 
                 registers = registers.Where(x => x.ProductName != null && x.ProductName.Contains(requestedProductName));
-                // TODO: add AND for store/model/band
             }
 
-            // Consult amount of pages for pagination 
-            if (registers is IQueryable<Register> queryrableRegisters)
+            if (!string.IsNullOrEmpty(requestedStoreName))
             {
-                // Use LINQ to asyncronic count 
-                amountPaginationPages = (int)Math.Ceiling(await registers.CountAsync() / (double)amountRegisterDisplayed);
-            } else
-            {
-                // if not Iquerable, count syncronic
-                var countRegisters = registers.Count();
-                amountPaginationPages = (int)Math.Ceiling(countRegisters / (double)amountRegisterDisplayed);
+                // Delimits registers with a where the properties are same 
+                registers = registers.Where(x => x.StoreName != null && x.StoreName.Contains(requestedStoreName));
             }
-            
 
-
-
+            Register = await registers.ToListAsync();
         }
 
-
-    }
+    } 
 }
