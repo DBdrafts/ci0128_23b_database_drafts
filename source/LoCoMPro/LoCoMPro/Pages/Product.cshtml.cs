f3db5b9eab0b8089849e5eb30db9c6874b6b18af
Omar <omar.camacho@ucr.ac.cr>
2023-09-30 20:35:09 -0600

    Ligera refactorización para obtener los atributos del producto solicitado.

--- source/LoCoMPro/LoCoMPro/Pages/Product.cshtml.cs
+++ source/LoCoMPro/LoCoMPro/Pages/Product.cshtml.cs
@@ -18,6 +18,12 @@ namespace LoCoMPro.Pages
         }
 
         /* List of the product that exist in the database */
+        public IList<Product> Product { get; set; } = default!;
+
+        /* List of the stores that exist in the database */
+        public IList<Store> Store { get; set; } = default!;
+
+        /* List of the registers that exist in the database */
         public IList<Register> Register { get; set; } = default!;
 
         /* Amount Register Displayed */
@@ -27,36 +33,37 @@ namespace LoCoMPro.Pages
         [BindProperty(SupportsGet = true)]
         public string? requestedProductName { get; set; }
         public string? requestedStoreName { get; set; }
-        public string? ProductBrand { get; set; }
-        public string? ProductModel { get; set; }
-        public string? ProductName { get; set; }
-        public string? CantonName { get; set; }
-        public string? ProvinceName { get; set; }
-
-
 
         public async Task OnGetAsync(string searchProductName, string searchStoreName)
         {
             // Requested Name to search
             requestedProductName = searchProductName;
             requestedStoreName = searchStoreName;
-            // Initial request that request all the registers in the database
-            var registers = from r in _context.Registers select r;
 
-            // If the name of th productResquested is not null
+            // Initial request for all the products in the database
+            var products = from r in _context.Products select r;
+
+            // Initial request for all the stores in the database
+            var stores = from r in _context.Stores select r;
+
+
+            // If the name of the productResquested is not null
             if (!string.IsNullOrEmpty(requestedProductName))
             {
                 // Delimits registers with a where the properties are same 
-                registers = registers.Where(x => x.ProductName != null && x.ProductName.Contains(requestedProductName));
+                products = products.Where(x => x.Name != null && x.Name.Contains(requestedProductName));
             }
 
+            // If the name of the storeResquested is not null
             if (!string.IsNullOrEmpty(requestedStoreName))
             {
                 // Delimits registers with a where the properties are same 
-                registers = registers.Where(x => x.StoreName != null && x.StoreName.Contains(requestedStoreName));
+                stores = stores.Where(x => x.Name != null && x.Name.Contains(requestedStoreName));
             }
 
-            Register = await registers.ToListAsync();
+            Product = await products.ToListAsync();
+            Store = await stores.ToListAsync();
+
         }
 
     } 
