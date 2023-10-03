2052a9ab372e2d51fe3d0c10dc8d4e52eda21e9a
Dwayne-T <dwaynetaylor763@gmail.com>
2023-10-03 15:08:56 -0600

    Se corrige el acceso a la base de datos

--- source/LoCoMPro/LoCoMPro/Pages/AddProductPage.cshtml.cs
+++ source/LoCoMPro/LoCoMPro/Pages/AddProductPage.cshtml.cs
@@ -134,13 +134,10 @@ namespace LoCoMPro.Pages
             // Check and create a new store if not exists
             var store = AddStoreRelation(storeName, cantonName, provinciaName);
             // Get category can be null
-            var category = _context.Categories.Find(chosenCategory);  
+            var category = _context.Categories.FirstOrDefault(c => c.CategoryName == chosenCategory);
 
-            var productToAdd = _context.Products.Find(productName);  // Get the product if exists in the context
-            var store = AddStoreRelation(storeName, cantonName, provinciaName);  // Check and create a new store if not exists
-            var category = _context.Categories.Find(chosenCategory);  // Get category/ can be null
-
-            if (productToAdd == null)  // If the product doesn't exists
+            // If the product doesn't exists
+            if (productToAdd == null)  
             {
                 // Create new product
                 productToAdd = new()
@@ -156,44 +153,15 @@ namespace LoCoMPro.Pages
                 }
                 // Add the product to the context
                 _context.Products.Add(productToAdd);
-
-            } else // if the product already exists
-            {   
-                if (category != null)
-                {   // Checks if the category-store (AsociatedWith) relationship already exists, if not, adds it
-                    string sqlCategoryQuery =
-                    "IF NOT EXISTS (SELECT * FROM AsociatedWith WHERE CategoryName = {0} AND ProductName = {1})\n" +
-                    "BEGIN\n" +
-                    "    INSERT INTO AsociatedWith (CategoryName, ProductName) VALUES ({0}, {1})\n" +
-                    "END";
-                    //var
-                    productToAdd.Categories!.Add(category);
-                    try
-                    {   // Apply SqlQuery
-                        _ = _context.Database.ExecuteSqlRaw(sqlCategoryQuery, chosenCategory!, productName);
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine($"An error occurred: {ex.Message}");
-                    }
-                }
             }
-            _context.SaveChanges();
+            // Checks if the category-store (AsociatedWith) relationship already exists, if not, adds it
+            else if (category != null && !productToAdd.Categories!.Contains(category))
+            {
+                productToAdd.Categories.Add(category);
+            }
 
             // Checks if the produc-store (Sells) relationship already exists, if not, adds it
-            string sqlQuery =
-                "IF NOT EXISTS (SELECT * FROM Sells WHERE ProductName = {0} AND StoreName = {1} AND ProvinceName = {2} AND CantonName = {3})\n" +
-                "BEGIN\n" +
-                "    INSERT INTO Sells (ProductName, StoreName, ProvinceName, CantonName) VALUES ({0}, {1}, {2}, {3})\n" +
-                "END";
-            try
-            {
-                _ = _context.Database.ExecuteSqlRaw(sqlQuery, productName, storeName, provinciaName, cantonName);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"An error occurred: {ex.Message}");
-            }
+            if (!productToAdd.Stores!.Contains(store)) productToAdd.Stores.Add(store);
 
             // Create new Register
             Register newRegister = new()
@@ -205,9 +173,11 @@ namespace LoCoMPro.Pages
                 Price = price,
                 Comment = comment
             };
-            _context.Registers.Add(newRegister);  // Add the product to the context
+            // Add the product to the context
+            _context.Registers.Add(newRegister);
 
-            await _context.SaveChangesAsync();  // Save all changes in the contextDB
+            // Save all changes in the contextDB
+            await _context.SaveChangesAsync();
 
             return RedirectToPage("/Index");
         }
@@ -236,10 +206,13 @@ namespace LoCoMPro.Pages
             }
             return attribute;
         }
+
+        // Suggests data for autocomplete on required inputs of page.
         public IActionResult OnGetAutocompleteSuggestions(string field, string term, string provinceName, string cantonName, string storeName)
         {
-            // Create a list withe the available suggestions, given the current inputs
+            // Create a list with the available suggestions, given the current inputs
             List<String> availableSuggestions = new List<string>() { "" };
+            // When aked for the autofill for store
             if (field == "#store")
             {
                 // Look for saved Stores in current location
@@ -248,12 +221,12 @@ namespace LoCoMPro.Pages
                     .Select(s => s.Name)
                     .ToList();
             }
+            // When asked for the autofill for product
             else if (field == "#productName")
             {
-                var sourceStore  = _context.Stores
-                                    .Include(s => s.Products)
-                                    .FirstOrDefault(s => s.Name == storeName && s.ProvinciaName == provinceName && s.CantonName == cantonName);
-                availableSuggestions = (sourceStore != null)? sourceStore.Products!.Select(p => p.Name).ToList() : new List<string>();
+                // Take the available sources form the store inventory or from the total number of produts.
+                availableSuggestions = _context.Products.Select(p => p.Name)
+                                                        .ToList();
             }
 
             // Filter suggestions based on the user's input
@@ -263,6 +236,7 @@ namespace LoCoMPro.Pages
 
             return new JsonResult(filteredSuggestions);
         }
+        // Autofills model, brand and category based on product name
         public IActionResult OnGetProductAutofillData(string productName)
         {
             var data = new Dictionary<string, string>();
@@ -278,13 +252,10 @@ namespace LoCoMPro.Pages
             // Get product Brand and model into the dictionary.
             data["#brand"] = productMatch.Brand ?? "";
             data["#model"] = productMatch.Model ?? "";
+            
+            // Get first Category result or null
             data["#category"] = (productMatch.Categories != null)? productMatch.Categories!.First().CategoryName : "";
 
-            // Get first Category result or null
-            var categoryName = _context.Database.SqlQueryRaw<string>(sqlQuery, parameters).FirstOrDefault();//.FirstOrDefault();
-            //FirstOrDefault();
-            data["#category"] = categoryName ?? "";
-            // Return Result
             return new JsonResult(data);
         }
     }
