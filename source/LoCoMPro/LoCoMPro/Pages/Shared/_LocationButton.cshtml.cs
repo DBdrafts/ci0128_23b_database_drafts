336f1d81481f27d8bf6bcf2e13a03171bf4cb601
DWAYNE JAMAL TAYLOR MONTERROSA <dwayne.taylor@ucr.ac.cr>
2023-10-04 17:14:33 -0600

    Se necesita cambar la funcionalidad del botón de ubicación através de javascript.

---
+++ source/LoCoMPro/LoCoMPro/Pages/Shared/_LocationButton.cshtml.cs
@@ -0,0 +1,90 @@
+﻿using LoCoMPro.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace LoCoMPro.Pages.Shared
+{
+    public class _LocationButtonModel : PageModel
+    {
+        private readonly LoCoMProContext _context;
+
+        public _LocationButtonModel(LoCoMProContext context)
+        {
+            _context = context;
+        }
+
+        public List<SelectListItem>? CategoryList { get; set; }
+        public List<SelectListItem>? ProvinciaList { get; set; }
+        public List<SelectListItem>? CantonList { get; set; }
+
+        // NOTE: The following 3 methods can be modularized, but to improve understanding, they were left as is
+        // maybe we can do it later
+
+
+        // Method for loading the list of categories from the database
+        private void LoadCategories()
+        {
+            // Retrieves all categories from the database and stores them
+            var categories = _context.Categories.ToList();
+
+            // Assigns the 'CategoryList' property a list of 'SelectListItem' items created from 'categories'
+            CategoryList = categories
+                .Select(category => new SelectListItem
+                {
+                    Value = category.CategoryName, // data that is sent to the server when the user selects an item from the list
+                    Text = category.CategoryName  // text that is displayed to the user in a dropdown list
+                })
+                .ToList();
+        }
+
+        // Method for loading the list of provinces from the database
+        private void LoadProvincias()
+        {
+            var provincias = _context.Provincias.ToList();
+            ProvinciaList = provincias
+                .Select(provincia => new SelectListItem
+                {
+                    Value = provincia.Name,
+                    Text = provincia.Name
+                })
+                .ToList();
+
+            // Checks if there is at least one province
+            if (provincias.Any())
+            {
+                LoadCantones(provincias.First().Name);
+            }
+        }
+
+        // Method for loading the list of cantones from the database
+        private void LoadCantones(string provincia)
+        {
+            var cantones = _context.Cantones
+                .Where(c => c.ProvinciaName == provincia)
+                .ToList();
+
+            CantonList = cantones
+                .Select(canton => new SelectListItem
+                {
+                    Value = canton.CantonName,
+                    Text = canton.CantonName
+                })
+            .ToList();
+        }
+
+        // Method called in response to an HTTP GET request to retrieve the list of cantones associated with a specific province
+        public JsonResult OnGetCantones(string provincia)
+        {
+            LoadCantones(provincia);
+            return new JsonResult(CantonList);
+        }
+
+        // Method called in response to an HTTP GET request
+        public void OnGet()
+        {
+            LoadCategories();
+            LoadProvincias();
+        }
+    }
+}
