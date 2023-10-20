99145ada0b23fba7571695870a042f833e5d42d5
Geanca Rivera <geancarlo.riverahernandez@ucr.ac.cr>
2023-10-20 16:24:02 -0600

    Se inicia popup de interacciones, funcionalidad de botón que cambia de color al hacer un reporte

---
+++ source/LoCoMPro/LoCoMPro/wwwroot/js/interactionsPopUp.js
@@ -0,0 +1,22 @@
+﻿function openInteractionsPopup() {
+    var popup = document.querySelector('.interactions-popup');
+    popup.style.display = 'block';
+}
+
+function closeInteractionsPopup() {
+    var popup = document.querySelector('.interactions-popup');
+    popup.style.display = 'none';
+}
+
+function toggleReport() {
+    var reportButton = document.getElementById('reportIcon');
+    if (reportIcon.src.endsWith('DesactiveReportIcon.svg')) {
+        reportIcon.src = '/img/ActiveReportIcon.svg';
+    } else {
+        reportIcon.src = '/img/DesactiveReportIcon.svg';
+    }
+}
+
+function saveInteractions() {
+    closeInteractionsPopup(); // Cierra el pop-up después de guardar.
+}
