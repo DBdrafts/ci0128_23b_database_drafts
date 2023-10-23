56aa464e5560f56ee6901f91b4a22fe6e7331b0d
Omar <omar.camacho@ucr.ac.cr>
2023-10-22 20:07:25 -0600

    Base inicial para la pagina de lista de productos agregada

---
+++ source/LoCoMPro/LoCoMPro/Pages/ProductListpage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LoCoMPro.Pages
+{
+    public class ProductListpageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
