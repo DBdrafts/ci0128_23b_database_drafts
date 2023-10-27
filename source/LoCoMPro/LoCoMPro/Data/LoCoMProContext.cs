eb00d5d9ecdea4e62a79243a0121b004929ff543
Dwayne-T <dwaynetaylor763@gmail.com>
2023-10-27 14:45:52 -0600

    Se agrega el tipo ubicación al modelo de cantón.

--- source/LoCoMPro/LoCoMPro/Data/LoCoMProContext.cs
+++ source/LoCoMPro/LoCoMPro/Data/LoCoMProContext.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using LoCoMPro.Models;
-using System.Reflection.Metadata;
 
 namespace LoCoMPro.Data
 {
@@ -64,102 +58,121 @@ namespace LoCoMPro.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            // Creating tables
-            modelBuilder.Entity<User>().ToTable("User");
-            modelBuilder.Entity<Product>().ToTable("Product");
-            modelBuilder.Entity<Store>().ToTable("Store");
-            modelBuilder.Entity<Register>().ToTable("Register");
-            modelBuilder.Entity<Canton>().ToTable("Canton");
-            modelBuilder.Entity<Provincia>().ToTable("Provincia");
 
             // Building relationships for Store
-            modelBuilder.Entity<Store>()
-                .HasOne(p => p.Location)
-                .WithMany(e => e.Stores)
-                .HasForeignKey(c => new { c.CantonName, c.ProvinciaName });
+            modelBuilder.Entity<Store>(entity =>
+            {
+                entity.ToTable("Store");
+
+                entity.HasOne(p => p.Location)
+                    .WithMany(e => e.Stores)
+                    .HasForeignKey(c => new { c.CantonName, c.ProvinciaName });
+
+                entity.HasMany(s => s.Products)
+                    .WithMany(p => p.Stores)
+                    .UsingEntity(
+                        "Sells"
+                        , l => l.HasOne(typeof(Product)).WithMany().HasForeignKey("ProductName")
+                        , r => r.HasOne(typeof(Store)).WithMany().HasForeignKey("StoreName", "CantonName", "ProvinceName"));
+
+                entity.Navigation(s => s.Products)
+                    .UsePropertyAccessMode(PropertyAccessMode.Property);
+
+                entity.Navigation(s => s.Registers)
+                    .UsePropertyAccessMode(PropertyAccessMode.Property);
+            });
+
 
             // Building relationships for Register
-            modelBuilder.Entity<Register>()
-                .HasOne(p => p.Store)
-                .WithMany(e => e.Registers)
-                .HasForeignKey(c => new { c.StoreName, c.CantonName, c.ProvinciaName});
+            modelBuilder.Entity<Register>(entity =>
+            {
+                entity.ToTable("Register");
 
-            // Sets 0 to NumCorrecions by default for Register
-            modelBuilder.Entity<Register>()
-                .Property(r => r.NumCorrections)
-                .HasDefaultValue(0);
+                entity.HasOne(p => p.Store)
+                    .WithMany(e => e.Registers)
+                    .HasForeignKey(c => new { c.StoreName, c.CantonName, c.ProvinciaName });
+
+                // Sets 0 to NumCorrecions by default for Register
+                entity.Property(r => r.NumCorrections)
+                    .HasDefaultValue(0);
 
 
-            // Building relationships for Register
-            modelBuilder.Entity<User>()
-                .HasOne(p => p.Location)
-                .WithMany(e => e.Users)
-                .HasForeignKey(c => new { c.CantonName, c.ProvinciaName });
+            });
+                
 
-            // Ignoring columns from default IdentityUser
-            modelBuilder.Entity<User>().Ignore(u => u.PhoneNumber);
-            modelBuilder.Entity<User>().Ignore(u => u.PhoneNumberConfirmed);
-            modelBuilder.Entity<User>().Ignore(u => u.EmailConfirmed);
-            modelBuilder.Entity<User>().Ignore(u => u.SecurityStamp);
-            modelBuilder.Entity<User>().Ignore(u => u.ConcurrencyStamp);
-            modelBuilder.Entity<User>().Ignore(u => u.TwoFactorEnabled);
-            modelBuilder.Entity<User>().Ignore(u => u.LockoutEnabled);
-            modelBuilder.Entity<User>().Ignore(u => u.LockoutEnd);
-            modelBuilder.Entity<User>().Ignore(u => u.AccessFailedCount);
+            // Building relationships for user
+            modelBuilder.Entity<User>(entity => {
+                entity.ToTable("User");
 
-            modelBuilder.Entity<Category>()
-                .HasMany(p => p.Products)
-                .WithMany(c => c.Categories)
-                .UsingEntity(
-                    "AsociatedWith"
-                    , l => l.HasOne(typeof(Product)).WithMany().HasForeignKey("ProductName")
-                    , r => r.HasOne(typeof(Category)).WithMany().HasForeignKey("CategoryName"));
+                entity.HasOne(p => p.Location)
+                    .WithMany(e => e.Users)
+                    .HasForeignKey(c => new { c.CantonName, c.ProvinciaName });
 
-            modelBuilder.Entity<Store>()
-                .HasMany(s => s.Products)
-                .WithMany(p => p.Stores)
-                .UsingEntity(
-                    "Sells"
-                    , l => l.HasOne(typeof(Product)).WithMany().HasForeignKey("ProductName")
-                    , r => r.HasOne(typeof(Store)).WithMany().HasForeignKey("StoreName", "CantonName", "ProvinceName"));
+                // Ignoring columns from default IdentityUser
+                entity.Ignore(u => u.PhoneNumber);
+                entity.Ignore(u => u.PhoneNumberConfirmed);
+                entity.Ignore(u => u.EmailConfirmed);
+                entity.Ignore(u => u.SecurityStamp);
+                entity.Ignore(u => u.ConcurrencyStamp);
+                entity.Ignore(u => u.TwoFactorEnabled);
+                entity.Ignore(u => u.LockoutEnabled);
+                entity.Ignore(u => u.LockoutEnd);
+                entity.Ignore(u => u.AccessFailedCount);
 
-            modelBuilder.Entity<Store>()
-                .Navigation(s => s.Products)
+                // Navigation
+                entity.Navigation(u => u.Registers)
+                    .UsePropertyAccessMode(PropertyAccessMode.Property);
+            });
+
+            // Building relationship for Product
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.ToTable("Product");
+
+                entity.Navigation(p => p.Stores)
+                    .UsePropertyAccessMode(PropertyAccessMode.Property);
+
+                entity.Navigation(p => p.Registers)
+                    .UsePropertyAccessMode(PropertyAccessMode.Property);
+
+                entity.Navigation(p => p.Categories)
+                    .UsePropertyAccessMode(PropertyAccessMode.Property);
+            });
+
+            // Building relationships for Canton
+            modelBuilder.Entity<Canton>(entity =>
+            {
+                entity.ToTable("Canton");
+
+                entity.Navigation(c => c.Stores)
                 .UsePropertyAccessMode(PropertyAccessMode.Property);
+            });
 
-            modelBuilder.Entity<Store>()
-                .Navigation(s => s.Registers)
-                .UsePropertyAccessMode(PropertyAccessMode.Property);
+            // Building relationships for Province
+            modelBuilder.Entity<Provincia>(entity =>
+            {
+                entity.ToTable("Provincia");
 
-            modelBuilder.Entity<Product>()
-                .Navigation(p => p.Stores)
-                .UsePropertyAccessMode(PropertyAccessMode.Property);
+                entity.Navigation(p => p.Cantones)
+                    .UsePropertyAccessMode(PropertyAccessMode.Property);
+            });
 
-            modelBuilder.Entity<Product>()
-                .Navigation(p => p.Registers)
-                .UsePropertyAccessMode(PropertyAccessMode.Property);
+            // Building relationships for Categoty
 
-            modelBuilder.Entity<Product>()
-                .Navigation(p => p.Categories)
-                .UsePropertyAccessMode(PropertyAccessMode.Property);
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.ToTable("Category");
 
-            modelBuilder.Entity<User>()
-                .Navigation(u => u.Registers)
-                .UsePropertyAccessMode(PropertyAccessMode.Property);
+                entity.HasMany(p => p.Products)
+                    .WithMany(c => c.Categories)
+                    .UsingEntity(
+                        "AsociatedWith"
+                        , l => l.HasOne(typeof(Product)).WithMany().HasForeignKey("ProductName")
+                        , r => r.HasOne(typeof(Category)).WithMany().HasForeignKey("CategoryName"));
 
-            modelBuilder.Entity<Category>()
-                .Navigation(c => c.Products)
-                .UsePropertyAccessMode(PropertyAccessMode.Property);
-
-            modelBuilder.Entity<Canton>()
-                .Navigation(c => c.Stores)
-                .UsePropertyAccessMode(PropertyAccessMode.Property);
-
-            modelBuilder.Entity<Provincia>()
-                .Navigation(p => p.Cantones)
-                .UsePropertyAccessMode(PropertyAccessMode.Property);
+                entity.Navigation(c => c.Products)
+                    .UsePropertyAccessMode(PropertyAccessMode.Property);
+            });
         }
-
     }
-
 }
