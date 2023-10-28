2ef4418b0e335e30ea5ea63f5de15eee23a4b39b
Omar <omar.camacho@ucr.ac.cr>
2023-10-28 17:01:39 -0600

    Cambio de imagen dinamica agregada en la seccion de productos

---
+++ source/LoCoMPro/LoCoMPro/wwwroot/js/ProductPage/imageChanger.js
@@ -0,0 +1,10 @@
+﻿
+// JavaScript para cambiar la imagen principal al poner el mouse sobre las imágenes pequeñas
+const smallImages = document.querySelectorAll('.small-image');
+const mainImage = document.getElementById('mainImage');
+
+smallImages.forEach(smallImage => {
+    smallImage.addEventListener('mouseover', () => {
+        mainImage.src = smallImage.src;
+    });
+});
