bac946314b7667020351e96c96f7ad0d68f60201
JulioAleRodri <JULIO.RODRIGUEZSALGUERA@ucr.ac.cr>
2023-10-28 14:21:58 -0600

    Modificando los elementos visuales de la página de lista de productos a los estándares

--- source/LoCoMPro/LoCoMPro/Pages/ProductListpage.cshtml.cs
+++ source/LoCoMPro/LoCoMPro/Pages/ProductListpage.cshtml.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace LoCoMPro.Pages
 {
-    public class ProductListpageModel : PageModel
+    public class ProductListPageModel : PageModel
     {
         public void OnGet()
         {
