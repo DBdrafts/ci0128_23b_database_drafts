18f1976f863582d633a46eb4ad0799db954ba49a
Omar <omar.camacho@ucr.ac.cr>
2023-10-30 11:51:42 -0600

    Primera version desmarcado de filtros funcional

---
+++ source/LoCoMPro/LoCoMPro/wwwroot/js/SearchPage/clearFilters.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    // manage the click in the button
+    $("#clear-filters").click(function () {
+        // Uncheck all boxes in the filter section
+        $("#filter-section input[type=checkbox]").prop("checked", false);
+        
+        // Trigger the filter function to reset the results
+        document.querySelectorAll('input[name="SelectedCantons"]').forEach(checkbox => {
+            checkbox.addEventListener('change', filterResults);
+        });
+    });
+});
--- source/LoCoMPro/LoCoMPro/wwwroot/js/SearchPage/filters.js
+++ source/LoCoMPro/LoCoMPro/wwwroot/js/SearchPage/filters.js
@@ -1,84 +1,89 @@
-﻿function stringContainsSubstringIgnoreCase(inputString, substringsArray) {
-    // Convert the input string to lowercase for case-insensitive comparison
-    const lowerCaseInput = inputString.toLowerCase();
+﻿    function stringContainsSubstringIgnoreCase(inputString, substringsArray) {
+        // Convert the input string to lowercase for case-insensitive comparison
+        const lowerCaseInput = inputString.toLowerCase();
 
-    // Check if any substring in the array is found in the lowercase input string
-    return substringsArray.some(substring => lowerCaseInput.includes(substring.toLowerCase()));
-}
-// .ready means that the function is executed when the document is fully loaded and ready to be manipulated
-document.addEventListener("DOMContentLoaded", function () {
-    const resultSection = document.getElementById("search-results");
-    // Function to update the URL and trigger a GET request
-    function filterResults() {
-        const selectedCategories = getSelectedCheckboxes('SelectedCategories');
-        const selectedProvinces = getSelectedCheckboxes('SelectedProvinces');
-        const selectedCantons = getSelectedCheckboxes('SelectedCantons');
-        notfound = document.getElementById("product-not-found");
-        filterSection = document.getElementById("filter-section");
+        // Check if any substring in the array is found in the lowercase input string
+        return substringsArray.some(substring => lowerCaseInput.includes(substring.toLowerCase()));
+    }
+    // .ready means that the function is executed when the document is fully loaded and ready to be manipulated
+    document.addEventListener("DOMContentLoaded", function () {
+        const resultSection = document.getElementById("search-results");
+        // Function to update the URL and trigger a GET request
+        function filterResults() {
+            const selectedCategories = getSelectedCheckboxes('SelectedCategories');
+            const selectedProvinces = getSelectedCheckboxes('SelectedProvinces');
+            const selectedCantons = getSelectedCheckboxes('SelectedCantons');
+            notfound = document.getElementById("product-not-found");
+            filterSection = document.getElementById("filter-section");
 
-        // Filter the resultBlocks based on the selected criteria
-        resultBlocks = queryResults.filter(block => {
-            const productName = block.querySelector('#register-product-name').getAttribute("value");
-            const province = block.querySelector('#register-province-name').getAttribute("value");
-            const canton = block.querySelector('#register-canton-name').getAttribute("value");
-            const categories = block.querySelector('#register-categories').getAttribute("value");
+            // Filter the resultBlocks based on the selected criteria
+            resultBlocks = queryResults.filter(block => {
+                const productName = block.querySelector('#register-product-name').getAttribute("value");
+                const province = block.querySelector('#register-province-name').getAttribute("value");
+                const canton = block.querySelector('#register-canton-name').getAttribute("value");
+                const categories = block.querySelector('#register-categories').getAttribute("value");
 
-            const meetsCategoryFilter = selectedCategories.length === 0 || stringContainsSubstringIgnoreCase(categories, selectedCategories);
-            const meetsProvinceFilter = selectedProvinces.length === 0 || selectedProvinces.includes(province);
-            const meetsCantonFilter = selectedCantons.length === 0 || selectedCantons.includes(canton);
+                const meetsCategoryFilter = selectedCategories.length === 0 || stringContainsSubstringIgnoreCase(categories, selectedCategories);
+                const meetsProvinceFilter = selectedProvinces.length === 0 || selectedProvinces.includes(province);
+                const meetsCantonFilter = selectedCantons.length === 0 || selectedCantons.includes(canton);
 
-            return meetsCategoryFilter && meetsProvinceFilter && meetsCantonFilter;
+                return meetsCategoryFilter && meetsProvinceFilter && meetsCantonFilter;
+            });
+
+            if (resultBlocks.length <= 0) {
+                notfound.hidden = false;
+                resultSection.hidden = true;
+                filterSection.hidden = true;
+                document.getElementById("results-count").textContent = "0";
+            } else {
+
+                // Sort the filtered resultBlocks
+                 sortResultBlocks("desc");
+
+                // Update the totalPages based on the filtered resultBlocks
+                totalPages = Math.ceil(resultBlocks.length / pageSize);
+
+                if (resultBlocks.length !== queryResults.length) {
+                    queryResults.forEach((block) => {
+                        block.style.display = 'none';
+                    });
+                }
+                // Show the first page
+                currentPage = 1;
+                showPage(currentPage);
+
+                // Update navigation buttons and page buttons
+                updateNavigationButtons();
+
+                notfound.hidden = true;
+                resultSection.hidden = false;
+                filterSection.hidden = false;
+            }
+        }
+
+        // Helper function to get an array of values from checked checkboxes
+        function getSelectedCheckboxes(checkboxName) {
+            return Array.from(document.querySelectorAll(`input[name="${checkboxName}"]:checked`)).map(checkbox => checkbox.value);
+        }
+
+        // Event handlers for when checkboxes are selected or deselected
+        document.querySelectorAll('input[name="SelectedCategories"]').forEach(checkbox => {
+            checkbox.addEventListener('change', filterResults);
         });
 
-        if (resultBlocks.length <= 0) {
-            notfound.hidden = false;
-            resultSection.hidden = true;
-            filterSection.hidden = true;
-            document.getElementById("results-count").textContent = "0";
-        } else {
+        document.querySelectorAll('input[name="SelectedProvinces"]').forEach(checkbox => {
+            checkbox.addEventListener('change', filterResults);
+        });
 
-            // Sort the filtered resultBlocks
-             sortResultBlocks("desc");
+        document.querySelectorAll('input[name="SelectedCantons"]').forEach(checkbox => {
+            checkbox.addEventListener('change', filterResults);
+        });
 
-            // Update the totalPages based on the filtered resultBlocks
-            totalPages = Math.ceil(resultBlocks.length / pageSize);
-
-            if (resultBlocks.length !== queryResults.length) {
-                queryResults.forEach((block) => {
-                    block.style.display = 'none';
-                });
-            }
-            // Show the first page
-            currentPage = 1;
-            showPage(currentPage);
-
-            // Update navigation buttons and page buttons
-            updateNavigationButtons();
-
-            notfound.hidden = true;
-            resultSection.hidden = false;
-            filterSection.hidden = false;
-        }
-    }
-
-    // Helper function to get an array of values from checked checkboxes
-    function getSelectedCheckboxes(checkboxName) {
-        return Array.from(document.querySelectorAll(`input[name="${checkboxName}"]:checked`)).map(checkbox => checkbox.value);
-    }
-
-    // Event handlers for when checkboxes are selected or deselected
-    document.querySelectorAll('input[name="SelectedCategories"]').forEach(checkbox => {
-        checkbox.addEventListener('change', filterResults);
+        // Initialize the filter on page load
+        filterResults();    
     });
 
-    document.querySelectorAll('input[name="SelectedProvinces"]').forEach(checkbox => {
-        checkbox.addEventListener('change', filterResults);
-    });
 
-    document.querySelectorAll('input[name="SelectedCantons"]').forEach(checkbox => {
-        checkbox.addEventListener('change', filterResults);
-    });
 
-    // Initialize the filter on page load
-    filterResults();
-});
+
+
