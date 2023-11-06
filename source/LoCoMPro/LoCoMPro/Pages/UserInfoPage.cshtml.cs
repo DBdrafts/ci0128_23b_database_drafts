0418e181493ae0c1347ce1efdbd5c5a02c5c772e
Omar <omar.camacho@ucr.ac.cr>
2023-11-05 18:12:19 -0600

    Estilos y visualizacion de la informacion del usuario modificada

--- source/LoCoMPro/LoCoMPro/Pages/UserInfoPage.cshtml.cs
+++ source/LoCoMPro/LoCoMPro/Pages/UserInfoPage.cshtml.cs
@@ -1,19 +1,69 @@
 using LoCoMPro.Data;
 using LoCoMPro.Utils;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
 
 namespace LoCoMPro.Pages
 {
     public class UserInfoPageModel : LoCoMProPageModel
     {
-        public UserInfoPageModel(LoCoMProContext context, IConfiguration configuration)
+        private readonly UserManager<User> _userManager;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserInfoPageModel(LoCoMProContext context, IConfiguration configuration,
+            UserManager<User> userManager, IHttpContextAccessor httpContextAccessor)
             : base(context, configuration)
         {
-
+            _userManager = userManager;
+            _httpContextAccessor = httpContextAccessor;
         }
-        public void OnGet()
+
+        /// <summary>
+        /// User in the Page
+        /// </summary>
+        public User UserInPage { get; set; }
+
+        /// <summary>
+        /// Name in the province
+        /// </summary>
+        public string Provincia { get; set; }
+
+        /// <summary>
+        /// Name of the canton
+        /// </summary>
+        public string Canton { get; set; }
+
+        public async Task OnGetAsync()
         {
+            // Stores the user in the page
+            UserInPage = await _userManager.GetUserAsync(User);
+
+            // Set the Province string name to display
+            if (UserInPage.ProvinciaName != null)
+            {
+                Provincia = UserInPage.ProvinciaName;
+            }
+            else
+            {
+                Provincia = "No agregada";
+            }
+            
+
+            // Set the Canton string name to display 
+            if (UserInPage.CantonName != null)
+            {
+                Canton = UserInPage.CantonName;
+            }
+            else
+            {
+                Canton = "No agregada";
+            }
+
+
         }
     }
 }
