62756ac800f7633bf6e1cffc87449ceb79280a4e
Omar <omar.camacho@ucr.ac.cr>
2023-11-06 13:53:49 -0600

    Guardar la geolocalizacion seleccionada por un usuario en el perfil del mismo.

--- source/LoCoMPro/LoCoMPro/Pages/UserInfoPage.cshtml.cs
+++ source/LoCoMPro/LoCoMPro/Pages/UserInfoPage.cshtml.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Http;
 using System.Threading.Tasks;
 using NetTopologySuite.Geometries;
+using Newtonsoft.Json;
+using System.Net;
 
 namespace LoCoMPro.Pages
 {
@@ -15,6 +17,14 @@ namespace LoCoMPro.Pages
         private readonly UserManager<User> _userManager;
         private readonly IHttpContextAccessor _httpContextAccessor;
 
+        /// <summary>
+        /// Creates a new UserInfoPageModel.
+        /// </summary>
+        /// <param name="context">DB Context to pull data from.</param>
+        /// <param name="configuration">Configuration for page.</param>
+        /// <param name="userManager">User manager to handle user permissions.</param>
+        /// <param name="httpContextAccessor">Allow access to the http context
+        // User Page constructor 
         public UserInfoPageModel(LoCoMProContext context, IConfiguration configuration,
             UserManager<User> userManager, IHttpContextAccessor httpContextAccessor)
             : base(context, configuration)
@@ -28,64 +38,98 @@ namespace LoCoMPro.Pages
         /// </summary>
         public User UserInPage { get; set; }
 
-
         /// <summary>
         /// Name in the province
         /// </summary>
         public string Provincia { get; set; }
-
-        public double latitud { get; set; }
-
-        public double longitud { get; set; }
-
 
         /// <summary>
         /// Name of the canton
         /// </summary>
         public string Canton { get; set; }
 
+        /// <summary>
+        /// double of latitud
+        /// </summary>
+        public double latitud { get; set; }
+
+        /// <summary>
+        /// Double of longitud
+        /// </summary>
+        public double longitud { get; set; }
+
+        /// <summary>
+        /// Selected geolocation
+        /// </summary>
+        public Point? geolocationSelected { get; set; }
+
+        /// <summary>
+        /// Selected geolocation
+        /// </summary>
+        public string? geolocationInPerfil { get; set; }
+
+        /// <summary>
+        /// GET HTTP request, initializes page values.
+        /// </summary>
         public async Task OnGetAsync()
         {
             // Stores the user in the page
             UserInPage = await _userManager.GetUserAsync(User);
 
             // Set the Province string name to display
-            if (UserInPage.ProvinciaName != null)
-            {
-                // Set the province as the data in the model
-                Provincia = UserInPage.ProvinciaName;
-            }
-            else
-            {   // Else alert to user
-                Provincia = "No agregada";
-            }
+            Provincia = UserInPage.ProvinciaName != null ? UserInPage.ProvinciaName : "No agregada";
 
             // Set the Canton string name to display 
-            if (UserInPage.CantonName != null)
-            {
-                // Set the Canton as the data in the model
-                Canton = UserInPage.CantonName;
-            }
-            else
-            {   // Else alert the user
-                Canton = "No agregada";
-            }
+            Canton = UserInPage.CantonName != null ? UserInPage.CantonName : "No agregada";
 
+            // Set the Canton string name to display 
+            geolocationSelected = UserInPage.Geolocation;
 
+            // Set the geolocation to display
+            geolocationInPerfil = UserInPage.Geolocation != null ? "Agregada" : "No agregada";
         }
 
-        public IActionResult OnPostUpdateProvince(double longitude, double latitude)
+        public void OnPostUpdateProvince(double longitude, double latitude)
         {
-            //UserInPage = await _userManager.GetUserAsync(User);
-
+            // set a new points to add to the user
             var coordinates = new Coordinate(longitude, latitude);
-            var geolocation = new Point(coordinates.X, coordinates.Y) { SRID = 4326 };
+            // stores the geolocaion in user var
+            geolocationSelected = new Point(coordinates.X, coordinates.Y) { SRID = 4326 };
 
+            // Set lat and lng
             latitud = latitude;
             longitud = longitude;
-            var response = new { message = "Provincia actualizada exitosamente" };
-            return new JsonResult(response);
+
+            // Wait to save the geolocation in the user profile
+            SaveGeolocationAsync().Wait();
         }
+
+        private async Task SaveGeolocationAsync()
+        {
+            // Get the actual user
+            var user = await _userManager.GetUserAsync(User);
+
+            // Set the value of the geolocation selected
+            user.Geolocation = geolocationSelected;
+
+            // Save the state to retun in javascript
+            var result = await _userManager.UpdateAsync(user);
+
+            if (result.Succeeded)
+            {
+                var response = new { message = "Provincia actualizada exitosamente" };
+                Response.StatusCode = (int)HttpStatusCode.OK;
+                await Response.WriteAsync(JsonConvert.SerializeObject(response));
+            }
+            else
+            {
+                var response = new { message = "Error al actualizar la geolocalización" };
+                Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                await Response.WriteAsync(JsonConvert.SerializeObject(response));
+            }
+        }
+
+
 
     }
 }
