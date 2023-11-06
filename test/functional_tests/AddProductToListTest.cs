e08a98ecfe3707365c06289b18854f53ef4c80a0
JulioAleRodri <JULIO.RODRIGUEZSALGUERA@ucr.ac.cr>
2023-11-06 16:18:34 -0600

    Se termina el test funcional de estudiante Julio Rodríguez, añadir producto a la lista

--- test/functional_tests/AddProductToListTest.cs
+++ test/functional_tests/AddProductToListTest.cs
@@ -1,4 +1,6 @@
-﻿using OpenQA.Selenium.Chrome;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +11,50 @@ namespace functional_tests
 {
     internal class AddProductToListTest
     {
+        WebDriver driver = new ChromeDriver();
 
         [SetUp]
         public void Setup()
         {
+            driver = new ChromeDriver();
+        }
+
+        // Test by Julio Alejandro Rodríguez Salguera
+        // Test from the Sprint 2
+        [Test]
+        public void AddProductToUserListTest()
+        {
+            // Arrange
+            driver.Navigate().GoToUrl("https://localhost:7119/Identity/Account/Login");
+
+            Login testPage = new Login(driver);
+
+            // Make the Login with a test user
+            var email = "julio444@ucr.ac.cr";
+            var password = "Julio444!";
+
+            testPage.SingIn(email, password);
+
+            // Go to the main page
+            testPage.ChangeUrl("https://localhost:7119");
+
+            // Search the product that is going to be add to the list
+            testPage.GoToProductPage("Leche");
+
+            // Act
+            driver.FindElement(By.Id("add-to-list")).Click();
+
+
+            // Assert
+            Assert.IsTrue(driver.FindElement(By.Id("add-to-list")).GetCssValue("display") == "none");
+            Assert.IsTrue(driver.FindElement(By.Id("remove-from-list")).GetCssValue("display") == "inline-block");
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
         }
     }
 }
--- test/functional_tests/Shared/MainPage.cs
+++ test/functional_tests/Shared/MainPage.cs
@@ -13,11 +13,22 @@ namespace functional_tests.Shared
         {
             url = "https://localhost:7119";
         }
+        public void GoToProductPage(String productToSearch)
+        {
+            SearchProduct(productToSearch);
+            driver.FindElement(By.Id("register-product-name")).Click();
+        }
 
         public void SearchProduct(string productToSearch)
         {
             SearchBar().SendKeys(productToSearch);
             SearchButton().Click();
         }
+
+        public void ChangeUrl(string newUrl)
+        {
+            driver.Navigate().GoToUrl(newUrl);
+        }
+
     }
 }
