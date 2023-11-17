fe07730f80a57b424aa8f589a0fb4d859588e2f3
JulioAleRodri <JULIO.RODRIGUEZSALGUERA@ucr.ac.cr>
2023-11-17 16:43:58 -0600

    Creando las bases para la interfaz del reporte de la lista de productos del usuario

---
+++ source/LoCoMPro/LoCoMPro/Pages/ListReportPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LoCoMPro.Pages
+{
+    public class ListReportPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
