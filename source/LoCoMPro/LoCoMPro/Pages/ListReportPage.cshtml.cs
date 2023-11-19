ad50d94726d99937ccc80016054c347fe621a302
JulioAleRodri <JULIO.RODRIGUEZSALGUERA@ucr.ac.cr>
2023-11-18 21:18:21 -0600

    Se realizan cambios visuales, se agrega la paginación y se agregan comentarios

--- source/LoCoMPro/LoCoMPro/Pages/ListReportPage.cshtml.cs
+++ source/LoCoMPro/LoCoMPro/Pages/ListReportPage.cshtml.cs
@@ -65,46 +65,68 @@ namespace LoCoMPro.Pages
         /// <returns></returns>
         public async void OnGetAsync()
         {
-            ObtainStoresThatSells();
+            // Obtains the list of stores that sells the products the list has
+            ObtainStoresListWithProducts();
         }
 
-
-        internal void ObtainStoresThatSells()
+        /// <summary>
+        /// Obtains the list of stores that sells the products the list has with the list of products the store has
+        /// </summary>
+        /// <returns></returns>
+        internal void ObtainStoresListWithProducts()
         {
+            // Obtains the product that are in the list
             WantedProducts = ObtainProductsFromList();
             
+            // For each product the add it to the list of the store
             foreach(var product in WantedProducts)
             {
                 var stores = from s in _context.Stores select s;
 
+                // Adds a product to the list of the stores that sells it
                 AddToProductStores(product, stores.Where(x => x.Products.Contains(product)).ToList());
             }
         }
-        
+
+        /// <summary>
+        /// Obtains the product that are in the list of the user
+        /// </summary>
+        /// <returns></returns>
         internal IList<Product> ObtainProductsFromList()
         {
             var productNames = UserProductList.Select(element => element.ProductName).ToList();
 
+            // Returns the list of products that are in the list
             return _context.Products.Where(p => productNames.Contains(p.Name)).ToList();
         }
 
+        /// <summary>
+        /// Adds a product to the list of the stores that sells it
+        /// </summary>
+        /// <param name="product"> Product to add to the list </param>
+        /// <param name="Stores"> Stores that sells the product </param>
+        /// <returns></returns>
         internal void AddToProductStores(Product product, List<Store> Stores)
         {
+            // For each stores that sells the product
             foreach(var store in Stores)
             {
-
                 var register = from r in _context.Registers select r;
 
-                // TODO(Traerse el de mejor nota, mmmmm)?
+                // Gets a register from the product
                 Register registerFromStore = register.Where(x => x.Product == product
                                                             && x.Store == store).FirstOrDefault()!;
 
+                // If the store was already stored
                 if(StoreProducts.ContainsKey(store))
                 {
+                    // Only add the product
                     StoreProducts[store].Add(registerFromStore);
                 }
+                // If wasn´t stored already
                 else
                 {
+                    // Add the store and add the product to the list
                     StoreProducts.Add(store, new List<Register> { registerFromStore });
                 }
             }
--- source/LoCoMPro/LoCoMPro/wwwroot/js/listReportPage.js
+++ source/LoCoMPro/LoCoMPro/wwwroot/js/listReportPage.js
@@ -1,22 +1,10 @@
-
-// TODO(Julio): Discutir esto de aquí
-/*document.addEventListener("DOMContentLoaded", function () {
-    var reportItems = document.querySelectorAll('.report-list-block');
-
-    reportItems.forEach(function (item, index) {
-        item.addEventListener('click', function () {
-            var productList = item.querySelector('.report-product-list');
-            productList.hidden = !productList.hidden;
-        });
-    });
-});*/
-
-
 document.addEventListener("DOMContentLoaded", function () {
     // Gets all the report blocks
     var reportItems = document.querySelectorAll('.report-list-block');
 
     reportItems.forEach(function (item, index) {
+        var arrowIcons = item.querySelector('.block-arrow');
+
         // When the block is clicked
         item.addEventListener('click', function () {
             // Gets the list of blocks
@@ -27,11 +15,53 @@ document.addEventListener("DOMContentLoaded", function () {
                 if (otherItem !== item) {
                     var otherProductList = otherItem.querySelector('.report-product-list');
                     otherProductList.hidden = true;
+
+                    var otherArrowIcons = otherItem.querySelector('.block-arrow');
+                    otherArrowIcons.querySelector('.fa-angle-up').hidden = true;
+                    otherArrowIcons.querySelector('.fa-angle-down').hidden = false;
                 }
             });
 
             // Open the product list
             productList.hidden = !productList.hidden;
+
+            arrowIcons.querySelector('.fa-angle-up').hidden = productList.hidden;
+            arrowIcons.querySelector('.fa-angle-down').hidden = !productList.hidden;
         });
     });
 });
+
+
+//document.addEventListener("DOMContentLoaded", function () {
+//    var reportItems = document.querySelectorAll('.report-list-block');
+
+//    reportItems.forEach(function (item, index) {
+//        var arrowIcons = item.querySelector('.block-arrow');
+//        var productList = item.querySelector('.report-product-list');
+
+//        // Establecer el estado inicial de las flechas
+//        arrowIcons.querySelector('.fa-angle-up').hidden = true;
+//        arrowIcons.querySelector('.fa-angle-down').hidden = false;
+
+//        item.addEventListener('click', function () {
+//            // Cerrar todos los demás elementos y reiniciar los íconos
+//            reportItems.forEach(function (otherItem) {
+//                if (otherItem !== item) {
+//                    var otherProductList = otherItem.querySelector('.report-product-list');
+//                    otherProductList.hidden = true;
+
+//                    var otherArrowIcons = otherItem.querySelector('.block-arrow');
+//                    otherArrowIcons.querySelector('.fa-angle-up').hidden = true;
+//                    otherArrowIcons.querySelector('.fa-angle-down').hidden = false;
+//                }
+//            });
+
+//            // Alternar la visibilidad del elemento actual
+//            productList.hidden = !productList.hidden;
+
+//            // Alternar la visibilidad de los íconos de flecha
+//            arrowIcons.querySelector('.fa-angle-up').hidden = !productList.hidden;
+//            arrowIcons.querySelector('.fa-angle-down').hidden = productList.hidden;
+//        });
+//    });
+//});
