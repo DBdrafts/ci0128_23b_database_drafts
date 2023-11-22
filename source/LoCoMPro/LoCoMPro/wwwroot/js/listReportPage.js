41ab59131e378160403bb479ecd552f485b1febd
JulioAleRodri <JULIO.RODRIGUEZSALGUERA@ucr.ac.cr>
2023-11-21 19:23:30 -0600

    Se realizan los últimos detalles y se cambia el string connection

--- source/LoCoMPro/LoCoMPro/wwwroot/js/listReportPage.js
+++ source/LoCoMPro/LoCoMPro/wwwroot/js/listReportPage.js
@@ -30,38 +30,3 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
-
-
-//document.addEventListener("DOMContentLoaded", function () {
-//    var reportItems = document.querySelectorAll('.report-list-block');
-
-//    reportItems.forEach(function (item, index) {
-//        var arrowIcons = item.querySelector('.block-arrow');
-//        var productList = item.querySelector('.report-product-list');
-
-//        // Establecer el estado inicial de las flechas
-//        arrowIcons.querySelector('.fa-angle-up').hidden = true;
-//        arrowIcons.querySelector('.fa-angle-down').hidden = false;
-
-//        item.addEventListener('click', function () {
-//            // Cerrar todos los demás elementos y reiniciar los íconos
-//            reportItems.forEach(function (otherItem) {
-//                if (otherItem !== item) {
-//                    var otherProductList = otherItem.querySelector('.report-product-list');
-//                    otherProductList.hidden = true;
-
-//                    var otherArrowIcons = otherItem.querySelector('.block-arrow');
-//                    otherArrowIcons.querySelector('.fa-angle-up').hidden = true;
-//                    otherArrowIcons.querySelector('.fa-angle-down').hidden = false;
-//                }
-//            });
-
-//            // Alternar la visibilidad del elemento actual
-//            productList.hidden = !productList.hidden;
-
-//            // Alternar la visibilidad de los íconos de flecha
-//            arrowIcons.querySelector('.fa-angle-up').hidden = !productList.hidden;
-//            arrowIcons.querySelector('.fa-angle-down').hidden = productList.hidden;
-//        });
-//    });
-//});
