f16b947d51ddab418eba809d475b0e78f1bb5b7e
Omar <omar.camacho@ucr.ac.cr>
2023-12-01 11:04:45 -0600

    Atributos de los usuarios que mas reportes reciben.

--- source/LoCoMPro/LoCoMPro/Pages/MostReportedPage.cshtml.cs
+++ source/LoCoMPro/LoCoMPro/Pages/MostReportedPage.cshtml.cs
@@ -1,12 +1,51 @@
+using LoCoMPro.Data;
+using LoCoMPro.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.Win32;
 
 namespace LoCoMPro.Pages
 {
-    public class MostReportedPageModel : PageModel
+    /// <summary>
+    /// Page model for see the Most Reported Users.
+    /// </summary>
+    [Authorize(Roles = "Moderator")]
+    public class MostReportedPageModel : LoCoMProPageModel
     {
-        public void OnGet()
+        private readonly UserManager<User> _userManager;
+
+        /// <summary>
+        /// List of users.
+        /// </summary>
+        public IList<User> Users { get; set; } = new List<User>();
+
+        /// <summary>
+        /// List of users that have made reports.
+        /// </summary>
+        public IList<User> UsersWhoMadeReports { get; set; } = new List<User>();
+
+        /// <summary>
+        /// The rating of the users.
+        /// </summary>
+        public IList<float> UserRatings { get; set; }
+
+        /// <summary>
+        /// List of reports.
+        /// </summary>
+        public IList<Report>? Reports { get; set; } = new List<Report>();
+
+        public MostReportedPageModel(LoCoMProContext context, IConfiguration configuration,
+           UserManager<User> userManager)
+           : base(context, configuration)
         {
+            _userManager = userManager;
         }
+
+
     }
 }
